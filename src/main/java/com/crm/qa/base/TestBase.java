f09e9c25a757495ae3cd66b655b7f9339244f82e
Remo Yukoff <basilisco.124@gmail.com>
2020-04-27 23:02:52 -0300

    added dependencies

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,11 +41,9 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
 			driver = new FirefoxDriver(); 
 		}
 		
